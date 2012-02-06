2e7b781ce0cba06fc97093dc89a3d3a8c76b2590
Chris Frisz <chris.frisz@gmail.com>
2012-02-06 10:01:20 -0500

    Adding tramp.clj for trampolining and algo.monads dependency.

--- project.clj
+++ project.clj
@@ -3,16 +3,18 @@
 ;; Written by Chris Frisz
 ;; 
 ;; Created  4 Feb 2012
-;; Last modified  5 Feb 2012
+;; Last modified  6 Feb 2012
 ;; 
 ;; Project declaration for clojure-tco. 
 ;;----------------------------------------------------------------------
 
-(defproject clojure-tco "0.1.0"
+(defproject clojure-tco "0.1.1"
   :description "Adding proper tail calls to Clojure."
   :url "https://github.iu.edu/cjfrisz/clojure-tco.git"
   :dependencies [[org.clojure/clojure
                   "1.3.0"]
                  [org.clojure/core.match
-                  "0.2.0-alpha9"]]
+                  "0.2.0-alpha9"]
+                 [com.id8/algo.monads
+                  "0.1.2"]]
   :repl-init clojure-tco.cps)
---
+++ src/clojure_tco/tramp.clj
@@ -0,0 +1,12 @@
+;;----------------------------------------------------------------------
+;; File tramp.clj
+;; Written by Chris Frisz
+;; 
+;; Created  6 Feb 2012
+;; Last modified  6 Feb 2012
+;; 
+;; Defines utilities for trampolining Clojure code.
+;;----------------------------------------------------------------------
+
+(ns clojure-tco.tramp
+  (:use [clojure.algo.monads :only [call/cc]]))

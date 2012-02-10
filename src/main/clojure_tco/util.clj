27a1aef3c8a75594254f12d3a71f1dedf93c3dfa
Chris Frisz <chris.frisz@gmail.com>
2012-02-09 22:52:58 -0500

    Adding file- and function-level documentation for util.clj.

--- src/main/clojure_tco/util.clj
+++ src/main/clojure_tco/util.clj
@@ -3,21 +3,33 @@
 ;; Written by Chris Frisz
 ;; 
 ;; Created  4 Feb 2012
-;; Last modified  5 Feb 2012
+;; Last modified  9 Feb 2012
 ;; 
 ;; Miscellaneous utilities for Clojure TCO.
-;;----------------------------------------------------------------------
+;;
+;; Defines the "new-var" function and associated helpers which
+;; generate semi-unique identifiers with a given symbol root
+;; symbol. This acts as a prettier gensym for performing the
+;; algorithms like CPS.
+;; ----------------------------------------------------------------------
 
 (ns clojure-tco.util)
 
+;; The separator which goes between the root symbol and number for
+;; identifiers.
 (def sep "!")
 
+;; The var-num which makes each new variable semi-unique.
 (def var-num (atom 0))
 
+;; Resets the var-num value to zero.
 (defn reset-var-num []
   (swap! var-num (fn [x] 0)))
 
-(defn new-var [sym]
+(defn new-var
+  "Takes a symbol and returns an identifier with the given symbol as
+  the root followed by a semi-unique suffix."
+  [sym]
   (let [new-var (symbol (str sym sep @var-num))]
     (do
       (swap! var-num inc)

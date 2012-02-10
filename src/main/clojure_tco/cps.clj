934e1a4dead6b0dd9bed7b8defa7716571b7125d
Chris Frisz <chris.frisz@gmail.com>
2012-02-09 22:43:41 -0500

    Adding file- and function-level documentation to teh CPSer.

--- src/main/clojure_tco/cps.clj
+++ src/main/clojure_tco/cps.clj
@@ -3,9 +3,29 @@
 ;; Written by Chris Frisz
 ;; 
 ;; Created  3 Feb 2012
-;; Last modified  6 Feb 2012
+;; Last modified  9 Feb 2012
 ;; 
-;; A CPSer for Clojure in Clojure.
+;; Defines CPS algorithm for Clojure expressions. The "cps" function
+;; takes a sequence representing a Clojure expression and returns
+;; another sequence reqresenting the CPS version of that expression
+;; which, when evaluated, takes a Clojure function representing a
+;; continuation and evaluates the original expression with respect to
+;; that continuation.
+;;
+;; The algorithm currently supports the following kinds of
+;; expressions:
+;;      Booleans
+;;      Symbols
+;;      Numbers
+;;      Functions using the "fn" form
+;;      Function applications involving aforementioned supported
+;;            functions.
+;;
+;; This implementation is intended for use as part of the Clojure TCO
+;; project for implementing tail-call optimization in Clojure.
+;;
+;; The CPS algorithm defined is based on one presented in "A
+;; First-Order One Pass CPS Algorithm" by Danvy, 2001.
 ;;----------------------------------------------------------------------
 
 (ns clojure-tco.cps
@@ -16,7 +36,10 @@
 
 (declare trivial? E S T)
 
-(defn trivial? [t]
+(defn trivial?
+  "Predicate that returns whether a given Clojure expression is
+  trivial with respect to the Olivier-style CPS algorithm."
+  [t]
   (match [t]
     [(b :when [true? false?])] true
     [(s :when symbol?)]        true
@@ -24,13 +47,19 @@
     [(['fn id-ls body] :seq)]  true
     :else                      false))
 
-(defn E [expr k]
+(defn E
+  "CPS function for an arbitrary Clojure expression with respect to
+  the Olivier-style CPS algorithm."
+  [expr k]
   (if (trivial? expr)
       (let [EXPR (T expr)]
         `(~k ~EXPR))
       (S expr k)))
 
-(defn S [expr k]
+(defn S
+  "CPS function for serious Clojure expressions with respect to the
+  Olivier-style CPS algorithm."
+  [expr k]
   (defn S-helper [expr k call]
     (if (nil? (seq expr))
         `(~@call ~k)
@@ -47,7 +76,10 @@
                 (S fst K))))))
   (S-helper expr k '()))
 
-(defn T [expr]
+(defn T
+  "CPS function for trivial Clojure expressions with respect to the
+  Olivier-style CPS algorithm."
+  [expr]
   (match [expr]
     [(s :when symbol?)] s
     [(n :when number?)] n
@@ -58,7 +90,12 @@
     :else (throw
            (Exception. (str "Invalid trivial expression: " expr)))))
 
-(defn cps [expr]
+(defn cps
+  "Entry-point function for the Olivier-style CPS algorithm. Returns
+  an function which takes a Clojure function representing a
+  continuation and evaluates the original expression with respect to
+  that continuation."
+  [expr]
   (do
     (reset-var-num)
     (let [k (new-var 'k)

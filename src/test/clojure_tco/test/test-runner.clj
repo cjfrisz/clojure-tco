8dc7045675dd872093929ee395b52fdc818a0d40
Chris Frisz <chris.frisz@gmail.com>
2012-02-10 00:10:09 -0500

    Drafting test-cps macro.

--- src/test/clojure_tco/test/test-runner.clj
+++ src/test/clojure_tco/test/test-runner.clj
@@ -3,7 +3,7 @@
 ;; Written by Chris Frisz
 ;; 
 ;; Created  6 Feb 2012
-;; Last modified  6 Feb 2012
+;; Last modified 10 Feb 2012
 ;; 
 ;; Defines macros for testing the correctness of clojure-tco
 ;; tranformations and checking the executation time and memory usage
@@ -11,4 +11,25 @@
 ;;----------------------------------------------------------------------
 
 (ns clojure-tco.test.test-runner
+  (:use [clojure.pprint])
+  (:use [clojure-tco.cps])
   (:use [clojure-tco.test.test-suite]))
+
+(defmacro test-cps
+  ([expr]
+     (let [original expr
+           cpsed (cps expr)]
+       (let [original-eval (eval expr)
+             cpsed-eval (eval (cps expr))]
+         (do
+           (print "Original: ") (pprint original)
+           (print "CPS: ") (pprint cpsed)
+           (println "Result:")
+           (if (= original-eval cpsed-eval)
+               (do
+                 (println "  Passed!")
+                 (println "  Value: " original-eval))
+               (do
+                 (println "  Failed!")
+                 (print "Original: ") (println original-eval)
+                 (print "CPSed: ") (println cpsed-eval))))))))

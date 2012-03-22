67c92b22660c77b83d9ec6ef5140a8fefea7616f
Chris Frisz <chris.frisz@gmail.com>
2012-03-22 00:11:34 -0400

    Teasing apart thunkify so it uses helper functions.

--- src/main/clojure_tco/thunkify.clj
+++ src/main/clojure_tco/thunkify.clj
@@ -3,7 +3,7 @@
 ;; Written by Chris Frisz
 ;; 
 ;; Created 21 Mar 2012
-;; Last modified 21 Mar 2012
+;; Last modified 22 Mar 2012
 ;; 
 ;; Primarily defines the function "thunkify," which takes a sequence
 ;; representing a Clojure expression (assumed to be CPSed) and returns
@@ -16,6 +16,9 @@
          :only (match)])
   (:use [clojure-tco.util
          :only (triv-op?)]))
+
+(declare
+ thunkify thunkify-fn thunkify-if thunkify-op thunkify-defn thunkify-app)
 
 ;;---------------------------------------------
 ;; THUNKIFY: Makes all functions return thunks
@@ -34,20 +37,46 @@
     [(:or true false)] expr
     [(n :when number?)] n
     [(s :when symbol?)] s
-    [(['fn fml* body] :seq)] `(~'fn ~fml* (~'fn [] ~(thunkify body)))
-    [(['if test conseq alt] :seq)]   (let [TEST (thunkify test)
-                                           CONSEQ (thunkify conseq)
-                                           ALT (thunkify alt)]
-                                       `(~'if ~TEST ~CONSEQ ~ALT))
-    [([(op :when triv-op?) & opnd*] :seq)] (let [OPND* (map thunkify opnd*)]
-                                             `(~op ~@OPND*))
-    [([(:or 'defn 'defn-) name fml* body] :seq)] (let [deftype (first expr)
-                                                       BODY (thunkify body)]
-                                                   `(~deftype ~name [~@fml*]
-                                                      (~'fn [] ~BODY)))
-    [([rator & rand*] :seq)] (let [rand-bl* (butlast rand*)
-                                   k (last rand*)
-                                   RATOR (thunkify rator)
-                                   RAND-BL* (map thunkify rand-bl*)]
-                               `(~RATOR ~@RAND-BL* ~k))
+    [(['fn fml* body] :seq)] (thunkify-fn fml* body)
+    [(['if test conseq alt] :seq)] (thunkify-if test conseq alt)
+    [([(op :when triv-op?) & opnd*] :seq)] (thunkify-op op opnd*)
+    [(['defn name fml* body] :seq)] (thunkify-defn name fml* body)
+    [([rator & rand*] :seq)] (thunkify-app rator rand*)
     :else (throw (Exception. (str "Invalid expression: " expr)))))
+
+(defn- thunkify-fn
+  "Helper function for thunkify that handles 'fn' expressions."
+  [fml* body]
+  (let [BODY (thunkify body)]
+    `(~'fn ~fml* (~'fn [] ~BODY))))
+
+(defn- thunkify-if
+  "Helper function for thunkify that handles 'if' expressions."
+  [test conseq alt]
+  (let [TEST (thunkify test)
+        CONSEQ (thunkify conseq)
+        ALT (thunkify alt)]
+    `(~'if ~TEST ~CONSEQ ~ALT)))
+
+(defn- thunkify-op
+  "Helper function for thunkify that handles simple operators (i.e. arithmetic,
+  relational, etc.)"
+  [op opnd*]
+   (let [OPND* (map thunkify opnd*)]
+     `(~op ~@OPND*)))
+
+(defn- thunkify-defn
+  "Helper function for thunkify that handles 'defn' expressions."
+  [name fml* body]
+  (let [BODY (thunkify body)]
+    `(~'defn ~name [~@fml*]
+       (~'fn [] ~BODY))))
+
+(defn- thunkify-app
+  "Helper function for thunkify that handles function application."
+  [rator rand*]
+  (let [rand-bl* (butlast rand*)
+        k (last rand*)
+        RATOR (thunkify rator)
+        RAND-BL* (map thunkify rand-bl*)]
+    `(~RATOR ~@RAND-BL* ~k)))

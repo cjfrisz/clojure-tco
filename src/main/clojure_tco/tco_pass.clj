4a18dc32103b57387684a7bb997861d50086ab77
Chris Frisz <chris.frisz@gmail.com>
2012-03-28 17:54:37 -0400

    Updating PTcoPass and TcoPass to allow default handlers.

--- src/main/clojure_tco/tco_pass.clj
+++ src/main/clojure_tco/tco_pass.clj
@@ -13,18 +13,23 @@
 
 (defprotocol PTcoPass
   "Protocol implemented by passes in the Clojure TCO compiler."
-  (register [this form handler] "Registers the language form as using the
-                                 handler function for a TCO pass.")
-  (run [this expr] "Given a language form, runs the handler for that form on the
-                    expression, returning the transformed expression."))
+  (register [this form handler]
+    "Registers the language form as using the handler function for a TCO pass.")
+  (register-default [this default]
+    "Registers a default handler to use when an expression doesn't match any
+    registered language forms.")
+  (run [this expr]
+    "Given a language form, runs the handler for that form on the expression,
+    returning the transformed expression."))
 
 (defrecord TcoPass [form->handler]
   PTcoPass
   (register [this form handler]
     (assoc (:form->handler this) form handler))
+  (register-default [this default]
+    (register this :default default))
   (run [this expr]
-    (let [form (some #(when (instance? % expr) (keys this)))]
-      (if-not (nil? form)
-        (let [handler ((:form->handler this) form)]
-          (handler expr))
-        (throw (Exception. (str "Expression " expr " isn't supported.")))))))
+    (let [form (or (some #(when (instance? % expr) (keys this)))
+                   :default)
+          handler ((:form->handler this) form)]
+      (handler expr))))

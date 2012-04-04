ec5d36cfe91d8860e5809e469975b70c0fca0476
Chris Frisz <chris.frisz@gmail.com>
2012-04-04 12:29:15 -0400

    Giving constructor argument to walk-expr better name in PWalkable.

--- src/main/clojure_tco/protocol/pwalkable.clj
+++ src/main/clojure_tco/protocol/pwalkable.clj
@@ -3,7 +3,7 @@
 ;; Written by Chris Frisz
 ;; 
 ;; Created 30 Mar 2012
-;; Last modified  2 Apr 2012
+;; Last modified  4 Apr 2012
 ;; 
 ;; Defines the base protocol for all expressions in the TCO compiler.
 ;;----------------------------------------------------------------------
@@ -12,6 +12,6 @@
 
 (defprotocol PWalkable
   "Protocol for TCO expressions that can be walked."
-  (walk-expr [this f c] [this f c args]
+  (walk-expr [this f ctor] [this f ctor args]
     "Applies the function f to the subforms of the argument expression,
     returning an expression created with the constructor."))

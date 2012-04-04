7797251bbddc6536e76ae6ba788a493add9edd5e
Chris Frisz <chris.frisz@gmail.com>
2012-04-04 00:48:01 -0400

    Updating Cont record type to implement PThunkify.

--- src/main/clojure_tco/expr/cont.clj
+++ src/main/clojure_tco/expr/cont.clj
@@ -3,14 +3,26 @@
 ;; Written by Chris Frisz
 ;; 
 ;; Created  1 Apr 2012
-;; Last modified  1 Apr 2012
+;; Last modified  4 Apr 2012
 ;; 
 ;; Defines the record types for continuations and continuation
 ;; application.
 ;;----------------------------------------------------------------------
 
-(ns clojure-tco.expr.cont)
+(ns clojure-tco.expr.cont
+  (:require [clojure-tco.protocol
+             [pthunkify :as pthunkify]]))
 
 (defrecord Cont [arg body])
 
 (defrecord AppCont [cont val])
+
+(def cont-thunkify {:thunkufy identity})
+
+(extend Cont
+  pthunkify/PThunkify
+  cont-thunkify)
+
+(extend AppCont
+  pthunkify/PThunkify
+  cont-thunkify)

f73ba278f495701bba90d7e8c9e1243cf0b7fdf1
Chris Frisz <chris.frisz@gmail.com>
2012-04-04 20:26:09 -0400

    Adding some stuff for App.

--- src/main/clojure_tco/expr/app.clj
+++ src/main/clojure_tco/expr/app.clj
@@ -3,7 +3,7 @@
 ;; Written by Chris Frisz
 ;; 
 ;; Created  2 Apr 2012
-;; Last modified  3 Apr 2012
+;; Last modified  4 Apr 2012
 ;; 
 ;; Defines the record types for function application in the Clojure
 ;; TCO compiler.
@@ -17,5 +17,11 @@
 
 (defrecord App [rator rand*]
   pwalkable/PWalkable
-  (walk-expr [this f c]
-    ))
+  (walk-expr [this f c] (pwalkable/walk-expr this f c nil))
+  (walk-expr [this f c arg*]
+    (let [RATOR (apply f (:rator this) arg*)
+          RAND* (map #(apply f % arg*) (:rand* this))]
+      (c RATOR RAND*)))
+
+  pcps/PCps
+  ())

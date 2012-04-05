f3d1895e11d40fcd1276c1ed7e8d4dcf99ead10e
Chris Frisz <chris.frisz@gmail.com>
2012-04-04 23:50:45 -0400

    Drafting App record type.

--- src/main/clojure_tco/expr/app.clj
+++ src/main/clojure_tco/expr/app.clj
@@ -13,15 +13,45 @@
   (:require [clojure-tco.protocol
              [pwalkable :as pwalkable]
              [pcps :as pcps] 
-             [pthunkify :as pthunkify]]))
+             [pthunkify :as pthunkify]]
+            [clojure-tco.expr.cont])
+  (:import [clojure_tco.expr.cont
+            Cont AppCont]))
 
 (defrecord App [rator rand*]
   pwalkable/PWalkable
-  (walk-expr [this f c] (pwalkable/walk-expr this f c nil))
-  (walk-expr [this f c arg*]
-    (let [RATOR (apply f (:rator this) arg*)
-          RAND* (map #(apply f % arg*) (:rand* this))]
-      (c RATOR RAND*)))
+    (walk-expr [this f c] (pwalkable/walk-expr this f c nil))
+    (walk-expr [this f c arg*]
+      (let [RATOR (apply f (:rator this) arg*)
+            RAND* (map #(apply f % arg*) (:rand* this))]
+        (c RATOR RAND*)))
 
   pcps/PCps
-  ())
+    (triv? [_] false)
+    (cps [this]
+      (throw
+       (Exception.
+        (str "Attempt to CPS App without continuation argument."))))
+    (cps [this k]
+      (let [RATOR (pcps/cps (:rator this) k)]
+        (letfn [(cps-app [pre-rand* post-rand* k]
+                  (if (nil? (seq pre-rand*))
+                      (let [RAND* (conj post-rand* k)]
+                        (App. RATOR RAND*))
+                      (let [fst (first pre-rand*)
+                            rst (rest pre-rand*)]
+                        (if (pcps/triv? fst)
+                            (let [FST (pcps/cps fst)
+                                  POST-RAND* (conj post-rand* FST)]
+                              (recur rst POST-RAND* k))
+                            (let [s (new-var 's)
+                                  POST-RAND* (conj POST-RAND* s)
+                                  RST (cps-app rst POST-RAND* k)
+                                  K (Cont. s RST)]
+                              (pcps/cps fst K))))))]
+          (cps-app (:rand* this) [] k))))
+
+  pthunkify/PThunkify
+    (thunkify [this]
+      (let [ctor #(App. %1 %2)]
+        (pwalkable/walk-expr this pthunkify/thunkify ctor))))

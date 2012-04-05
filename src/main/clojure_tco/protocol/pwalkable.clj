2e10eb5abce86f5301cc1337ee67788b49518a1c
Chris Frisz <chris.frisz@gmail.com>
2012-04-05 18:44:21 -0400

    Removing unnecessary version of walk-expr using additional arg.

--- src/main/clojure_tco/protocol/pwalkable.clj
+++ src/main/clojure_tco/protocol/pwalkable.clj
@@ -3,15 +3,15 @@
 ;; Written by Chris Frisz
 ;; 
 ;; Created 30 Mar 2012
-;; Last modified  4 Apr 2012
+;; Last modified  5 Apr 2012
 ;; 
-;; Defines the base protocol for all expressions in the TCO compiler.
+;; Defines the protocol for TCO expressions which can be walked
 ;;----------------------------------------------------------------------
 
 (ns clojure-tco.protocol.pwalkable)
 
 (defprotocol PWalkable
   "Protocol for TCO expressions that can be walked."
-  (walk-expr [this f ctor] [this f ctor args]
+  (walk-expr [this f ctor]
     "Applies the function f to the subforms of the argument expression,
     returning an expression created with the constructor."))

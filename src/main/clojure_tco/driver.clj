5134851738aca49d22f8ed8c3594078afa9365bc
Chris Frisz <chris.frisz@gmail.com>
2012-04-11 17:33:07 -0400

    Adding driver file for the TCO compiler.

---
+++ src/main/clojure_tco/driver.clj
@@ -0,0 +1,12 @@
+;;----------------------------------------------------------------------
+;; File driver.clj
+;; Written by Chris Frisz
+;; 
+;; Created 11 Apr 2012
+;; Last modified 11 Apr 2012
+;; 
+;; Defines the driver for the Clojure TCO compiler.
+;;----------------------------------------------------------------------
+
+(ns clojure-tco.driver
+  (:require))

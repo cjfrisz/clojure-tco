6d4e8245b2928bce6494f88d0e5ba4d104da70fb
Chris Frisz <chris.frisz@gmail.com>
2012-04-11 00:25:17 -0400

    Adding more tests for CPS with record types.

--- src/test/clojure_tco/test/cps.clj
+++ src/test/clojure_tco/test/cps.clj
@@ -10,12 +10,15 @@
 ;;----------------------------------------------------------------------
 
 (ns clojure-tco.test.cps
-  (:use clojure.test)
+  (:use [clojure.test]
+        [clojure.pprint])
   (:require [clojure-tco.expr
              app atomic cont defn fn if simple-op]
             [clojure-tco.protocol
              [pcps-srs :as srs]
-             [pcps-triv :as triv]])
+             [pcps-triv :as triv]]
+            [clojure-tco.util
+             [new-var :as nv]])
   (:import [clojure_tco.expr.app
             App]
            [clojure_tco.expr.atomic
@@ -33,50 +36,105 @@
 
 (let [test-bool (Bool. true)]
   (deftest atomic-bool
-    (is (true? (extends? triv/PCpsTriv (type test-bool))))
-    (is (false? (extends? srs/PCpsSrs (type test-bool))))
+    (is (extends? triv/PCpsTriv (type test-bool)))
+    (is (not (extends? srs/PCpsSrs (type test-bool))))
     (is (= (triv/cps test-bool) test-bool))))
 
 (let [test-num (Num. true)]
   (deftest atomic-num
-    (is (true? (extends? triv/PCpsTriv (type test-num))))
-    (is (false? (extends? srs/PCpsSrs (type test-num))))
+    (is (extends? triv/PCpsTriv (type test-num)))
+    (is (not (extends? srs/PCpsSrs (type test-num))))
     (is (= (triv/cps test-num) test-num))))
 
 (let [test-sym (Sym. true)]
   (deftest atomic-sym
-    (is (true? (extends? triv/PCpsTriv (type test-sym))))
-    (is (false? (extends? srs/PCpsSrs (type test-sym))))
+    (is (extends? triv/PCpsTriv (type test-sym)))
+    (is (not (extends? srs/PCpsSrs (type test-sym))))
     (is (= (triv/cps test-sym) test-sym))))
 
 (let [test-var (Var. true)]
   (deftest atomic-var
-    (is (true? (extends? triv/PCpsTriv (type test-var))))
-    (is (false? (extends? srs/PCpsSrs (type test-var))))
+    (is (extends? triv/PCpsTriv (type test-var)))
+    (is (not (extends? srs/PCpsSrs (type test-var))))
     (is (= (triv/cps test-var) test-var))))
 
 (let [test-fn-triv (Fn. [(Var. 'x)] (Var. 'x))]
   (deftest fn-triv
-    (is (true? (extends? triv/PCpsTriv (type test-fn-triv))))
-    (is (false? (extends? srs/PCpsSrs (type test-fn-triv))))
+    (is (extends? triv/PCpsTriv (type test-fn-triv)))
+    (is (not (extends? srs/PCpsSrs (type test-fn-triv))))
     (is (= (count (:fml* test-fn-triv)) 1))
     (let [test-fn-cps (triv/cps test-fn-triv)]
       (is (not (= test-fn-triv test-fn-cps)))
       (is (= (count (:fml* test-fn-cps)) 2))
-      (is (= (type (:body test-fn-cps)) clojure_tco.expr.cont.AppCont)))))
+      (is (instance? AppCont (:body test-fn-cps))))))
 
 (let [test-if-triv (IfTriv. (Num. 3) (Num. 4) (Num. 5))]
   (deftest if-triv
-    (is (true? (extends? triv/PCpsTriv (type test-if-triv))))
-    (is (false? (extends? srs/PCpsSrs (type test-if-triv))))
+    (is (extends? triv/PCpsTriv (type test-if-triv)))
+    (is (not (extends? srs/PCpsSrs (type test-if-triv))))
     (let [test-if-cps (triv/cps test-if-triv)]
       (is (= (:test test-if-triv) (:test test-if-cps)))
       (is (= (:conseq test-if-triv) (:conseq test-if-cps)))
       (is (= (:alt test-if-triv) (:alt test-if-cps)))
-      (is (= (type test-if-cps) clojure_tco.expr.if.IfCps)))))
+      (is (instance? IfCps test-if-cps)))))
 
 (let [test-defn (Defn. 'id (Fn. [(Var. 'x)] (Var. 'x)))]
   (deftest defn-triv
-    (is (true? (extends? triv/PCpsTriv (type test-defn))))
-    (is (false? (extends? srs/PCpsSrs (type test-defn))))
-    (is (= (triv/cps (:func test-defn)) (:func (triv/cps test-defn))))))
+    (is (extends? triv/PCpsTriv (type test-defn)))
+    (is (not (extends? srs/PCpsSrs (type test-defn))))
+    (let [test-defn-cps (triv/cps test-defn)]
+      (is (instance? AppCont (:body (:func test-defn-cps)))))))
+
+(let [test-op (SimpleOpTriv. '+ [(Num. 3) (Num. 4) (Num. 5)])]
+  (deftest simple-op-triv
+    (is (extends? triv/PCpsTriv (type test-op)))
+    (is (not (extends? srs/PCpsSrs (type test-op))))
+    (let [test-op-cps (triv/cps test-op)]
+      (for [opnd (:opnd* test-op)
+            opnd-cps (:opnd* test-op-cps)]
+        (is (= opnd opnd-cps))))))
+
+(let [test-app (App. (Fn. [(Var. 'x)] (Var. 'x)) [(Num. 5)])]
+  (deftest app
+    (is (extends? srs/PCpsSrs (type test-app)))
+    (let [k (nv/new-var 'k)
+          app-cps (srs/cps test-app k)]
+      (is (instance? App app-cps)))))
+
+(let [test-if-srs (IfSrs. (SimpleOpTriv. 'zero? [(Var. 'x)])
+                          (App. (Fn. [(Var. 'x)] (Var. 'x)) [(Num. 5)])
+                          (Num. 12))]
+  (deftest if-srs
+    (is (extends? srs/PCpsSrs (type test-if-srs)))
+    (is (extends? triv/PCpsTriv (type (:test test-if-srs))))
+    (is (extends? srs/PCpsSrs (type (:conseq test-if-srs))))
+    (is (extends? triv/PCpsTriv (type (:alt test-if-srs))))
+    (let [k (nv/new-var 'k)
+          test-if-cps (srs/cps test-if-srs k)]
+      #_(pprint test-if-cps))))
+
+(let [test-if-srs (IfSrs. (App. (Fn. [(Var. 'x)]
+                                     (SimpleOpTriv. 'zero? [(Var. 'x)]))
+                                [(Num. 35)])
+                          (App. (Fn. [(Var. 'x)] (Var. 'x)) [(Num. 5)])
+                          (Num. 12))]
+  (deftest if-srs2
+    (is (extends? srs/PCpsSrs (type test-if-srs)))
+    (is (extends? srs/PCpsSrs (type (:test test-if-srs))))
+    (is (extends? srs/PCpsSrs (type (:conseq test-if-srs))))
+    (is (extends? triv/PCpsTriv (type (:alt test-if-srs))))
+    (let [k (nv/new-var 'k)
+          test-if-cps (srs/cps test-if-srs k)]
+      #_(pprint test-if-cps))))
+
+(let [test-op-srs (SimpleOpSrs. '+ [(Num. 3)
+                                    (App. (Fn. [(Var. 'x)] (Var. 'x)) [(Num. 5)])
+                                    (Num. 5)])]
+  (deftest simple-op-srs
+    (is (extends? srs/PCpsSrs (type test-op-srs)))
+    (is (extends? triv/PCpsTriv (type (nth (:opnd* test-op-srs) 0))))
+    (is (extends? srs/PCpsSrs (type (nth (:opnd* test-op-srs) 1))))
+    (is (extends? triv/PCpsTriv (type (nth (:opnd* test-op-srs) 2))))
+    (let [k (nv/new-var 'k)
+          test-op-cps (srs/cps test-op-srs k)]
+      (pprint test-op-cps))))

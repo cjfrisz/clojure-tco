8a6341ca33e3e13741f6eba70ed1bfa844addccb
Chris Frisz <chris.frisz@gmail.com>
2012-04-17 05:48:17 -0400

    Rewriting parser in terms of secondary constructors.

--- src/main/clojure_tco/parse.clj
+++ src/main/clojure_tco/parse.clj
@@ -3,7 +3,7 @@
 ;; Written by Chris Frisz
 ;; 
 ;; Created 10 Apr 2012
-;; Last modified 16 Apr 2012
+;; Last modified 17 Apr 2012
 ;; 
 ;; Defines the parser for the Clojure TCO compiler.
 ;;----------------------------------------------------------------------
@@ -12,22 +12,15 @@
   (:use [clojure.core.match
          :only (match)])
   (:require [clojure-tco.expr
-             app atomic defn fn if simple-op]
+             [app :as app]
+             [atomic :as atomic]
+             [defn :as defn]
+             [fn :as fn]
+             [if :as if]
+             [simple-op :as simple-op]]
             [clojure-tco.protocol
              [pcps-srs :as srs]
-             [pcps-triv :as triv]])
-  (:import [clojure_tco.expr.app
-            App]
-           [clojure_tco.expr.atomic
-            Atomic]
-           [clojure_tco.expr.defn
-            Defn]
-           [clojure_tco.expr.fn
-            Fn]
-           [clojure_tco.expr.if
-            IfCps IfSrs IfTriv]
-           [clojure_tco.expr.simple_op
-            SimpleOpCps SimpleOpSrs SimpleOpTriv]))
+             [pcps-triv :as triv]]))
 
 (declare parse parse-fn parse-defn parse-if parse-op parse-app simple-op?)
 
@@ -37,10 +30,10 @@
   records."
   [expr]
   (match [expr]
-    [(:or true false)] (Atomic. expr)
-    [(n :when number?)] (Atomic. n)
-    [(['quote s] :seq)] (Atomic. `(quote ~s))
-    [(v :when symbol?)] (Atomic. v)
+    [(:or true false)] (atomic/make-atomic expr)
+    [(n :when number?)] (atomic/make-atomic n)
+    [(['quote s] :seq)] (atomic/make-atomic `(quote ~s))
+    [(v :when symbol?)] (atomic/make-atomic v)
     [(['fn fml* body] :seq)] (parse-fn fml* body)
     [(['defn name (fml* :when vector?) body] :seq)] (let [func* `((~fml* ~body))]
                                                       (parse-defn name func*)) 
@@ -56,7 +49,7 @@
   (let [FML* (map parse fml*)
         FML* (into [] FML*)
         BODY (parse body)]
-    (Fn. FML* BODY)))
+    (fn/make-fn FML* BODY)))
 
 (defn- parse-defn
   "Helper function for parse that handles 'defn' expression."
@@ -68,9 +61,9 @@
                       body (first (nfirst func*))
                       func (parse-fn fml* body)]
                   (recur (next func*) (conj out* func)))))]
-    (let [NAME (Atomic. name)
+    (let [NAME (atomic/make-atomic name)
           FUNC* (parse-func* func* [])]
-      (Defn. NAME FUNC*))))
+      (defn/make-defn NAME FUNC*))))
 
 (defn- parse-if
   "Helper function for parse that handles 'if' expressions."
@@ -79,8 +72,8 @@
         CONSEQ (parse conseq)
         ALT (parse alt)]
     (if (every? #(extends? triv/PCpsTriv (type %)) [TEST CONSEQ ALT])
-        (IfTriv. TEST CONSEQ ALT)
-        (IfSrs. TEST CONSEQ ALT))))
+        (if/make-if-triv TEST CONSEQ ALT)
+        (if/make-if-srs TEST CONSEQ ALT))))
 
 (defn- parse-op
   "Helper function for parse that handles simple op expressions (e.g. +, -,
@@ -89,8 +82,8 @@
   (let [OPND* (map parse opnd*)]
     (let [OPND* (into [] OPND*)]
       (if (every? #(extends? triv/PCpsTriv (type %)) OPND*)
-          (SimpleOpTriv. op OPND*)
-          (SimpleOpSrs. op OPND*)))))
+          (simple-op/make-simple-op-triv op OPND*)
+          (simple-op/make-simple-op-srs op OPND*)))))
 
 (defn- parse-app
   "Helper function for parse that handles application."
@@ -98,7 +91,7 @@
   (let [RATOR (parse rator)
         RAND* (map parse rand*)]
     (let [RAND* (into [] RAND*)]
-      (App. RATOR RAND*))))
+      (app/make-app RATOR RAND*))))
 
 (defn- simple-op?
   "Predicate returning whether op is a simple, value-returning operator."

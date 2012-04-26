7aadfd355a9168862fa0fc096245a1f2494ee7fa
Chris Frisz <chris.frisz@gmail.com>
2012-04-26 16:53:24 -0400

    Altering ctco.clj to use the unified protocol.clj file.

--- src/main/ctco.clj
+++ src/main/ctco.clj
@@ -3,7 +3,7 @@
 ;; Written by Chris Frisz
 ;; 
 ;; Created 11 Apr 2012
-;; Last modified 23 Apr 2012
+;; Last modified 26 Apr 2012
 ;; 
 ;; Defines the driver for the Clojure TCO compiler.
 ;;----------------------------------------------------------------------
@@ -13,12 +13,7 @@
              [cont defn fn]]
             [ctco.mini-passes :as mp]
             [ctco.parse :as parse]
-            [ctco.protocol
-             [pabstract-k :as pabs-k]
-             [pcps-srs :as srs]
-             [pcps-triv :as triv]
-             [pemit :as pemit]
-             [pthunkify :as pthunkify]]
+            [ctco.protocol :as proto]
             [ctco.util.new-var :as nv])
   (:import [ctco.expr.cont
             Cont AppContAbs]
@@ -38,10 +33,10 @@
         apply-k (nv/new-var 'apply-k)
         flag (nv/new-var 'flag)]
     (letfn [(apply-cps [expr]
-              (if (extends? srs/PCpsSrs (type expr))
+              (if (extends? proto/PCpsSrs (type expr))
                   (let [k (nv/new-var 'k)]
-                    (srs/cps expr k))
-                  (triv/cps expr)))
+                    (proto/cps-srs expr k))
+                  (proto/cps-triv expr)))
             (wrap-expr [expr]
               (if (instance? Defn expr)
                   expr
@@ -50,12 +45,12 @@
                     (Cont. k app))))]
       (let [expr (parse/parse expr)
             expr (apply-cps expr)
-            expr (pabs-k/abstract-k expr apply-k)
-            expr (pthunkify/thunkify expr)
+            expr (proto/abstract-k expr apply-k)
+            expr (proto/thunkify expr)
             expr (wrap-expr expr)
             expr (mp/overload expr tramp flag)
             expr (mp/make-flag expr flag)
             expr (mp/make-apply-k expr apply-k)
             expr (mp/make-trampoline expr tramp)]
-        (pemit/emit expr)))))
+        (proto/emit expr)))))
 

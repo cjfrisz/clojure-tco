fd2b16d561937a5cfc320192f755694ac9d7d5cf
Chris Frisz <chris.frisz@gmail.com>
2012-04-27 06:50:16 -0400

    Removing cps-expr from util.

--- src/main/ctco/util.clj
+++ src/main/ctco/util.clj
@@ -3,7 +3,7 @@
 ;; Written by Chris Frisz
 ;; 
 ;; Created 26 Apr 2012
-;; Last modified 26 Apr 2012
+;; Last modified 27 Apr 2012
 ;; 
 ;; Defines miscellaneous utility functions for use in CTCO. These
 ;; include:
@@ -19,21 +19,6 @@
              Atomic]
             [ctco.expr.cont
              Cont AppCont]))
-
-(declare cps-expr new-var)
-
-(defn cps-expr
-  "Applies the appropriate CPS transformation to the given expression according
-  to whether it implements PCpsSrs or PCpsTriv."
-  ([expr]
-     (condp extends? (type expr)
-       proto/PCpsSrs  (proto/cps-srs expr (::new-var 'k))
-       proto/PCpsTriv (proto/cps-triv expr)))
-  ([expr k]
-     (condp extends? (type expr)
-       proto/PCpsSrs  (proto/cps-srs expr k)
-       proto/PCpsTriv (let [EXPR (proto/cps-triv expr)]
-                        (AppCont. k EXPR)))))
 
 (defn new-var
   "Returns a unique variable for use in the TCO compiler either with a given

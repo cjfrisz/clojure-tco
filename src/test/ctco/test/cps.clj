def203a1d776358c601a574e4e90a1c7f193c2e7
Chris Frisz <chris.frisz@gmail.com>
2012-04-28 07:37:44 -0400

    Correcting tests for new structure of CTCO files.

--- src/test/ctco/test/cps.clj
+++ src/test/ctco/test/cps.clj
@@ -3,76 +3,73 @@
 ;; Written by Chris Frisz
 ;; 
 ;; Created 10 Apr 2012
-;; Last modified 21 Apr 2012
+;; Last modified 28 Apr 2012
 ;; 
 ;; Testing for the CPSer in the record+protocol'd version of the TCO
 ;; compiler.
 ;;----------------------------------------------------------------------
 
-(ns bbc.test.cps
+(ns ctco.test.cps
   (:use [clojure.test]
         [clojure.pprint])
-  (:require [bbc.expr
+  (:require [ctco.expr
              app atomic cont defn fn if simple-op]
-            [bbc.protocol
-             [pcps-srs :as srs]
-             [pcps-triv :as triv]]
-            [bbc.util
-             [new-var :as nv]])
-  (:import [bbc.expr.app
+            [ctco.protocol :as proto]
+            [ctco.util :as util])
+  (:import [ctco.expr.app
             App]
-           [bbc.expr.atomic
+           [ctco.expr.atomic
             Atomic]
-           [bbc.expr.cont
+           [ctco.expr.cont
             Cont AppCont]
-           [bbc.expr.defn
+           [ctco.expr.defn
             Defn]
-           [bbc.expr.fn
+           [ctco.expr.fn
             Fn]
-           [bbc.expr.if
+           [ctco.expr.if
             IfCps IfSrs IfTriv]
-           [bbc.expr.simple_op
+           [ctco.expr.simple_op
             SimpleOpCps SimpleOpSrs SimpleOpTriv]))
 
 (let [test-bool (Atomic. true)]
   (deftest atomic-bool
-    (is (extends? triv/PCpsTriv (type test-bool)))
-    (is (not (extends? srs/PCpsSrs (type test-bool))))
-    (is (= (triv/cps test-bool) test-bool))))
+    (is (extends? proto/PCpsTriv (type test-bool)))
+    (is (not (extends? proto/PCpsSrs (type test-bool))))
+    (is (= (proto/cps-triv test-bool) test-bool))))
 
 (let [test-num (Atomic. 5)]
   (deftest atomic-num
-    (is (extends? triv/PCpsTriv (type test-num)))
-    (is (not (extends? srs/PCpsSrs (type test-num))))
-    (is (= (triv/cps test-num) test-num))))
+    (is (extends? proto/PCpsTriv (type test-num)))
+    (is (not (extends? proto/PCpsSrs (type test-num))))
+    (is (= (proto/cps-triv test-num) test-num))))
 
 (let [test-sym (Atomic. '(quote s))]
   (deftest atomic-sym
-    (is (extends? triv/PCpsTriv (type test-sym)))
-    (is (not (extends? srs/PCpsSrs (type test-sym))))
-    (is (= (triv/cps test-sym) test-sym))))
+    (is (extends? proto/PCpsTriv (type test-sym)))
+    (is (not (extends? proto/PCpsSrs (type test-sym))))
+    (is (= (proto/cps-triv test-sym) test-sym))))
 
 (let [test-var (Atomic. 'x)]
   (deftest atomic-var
-    (is (extends? triv/PCpsTriv (type test-var)))
-    (is (not (extends? srs/PCpsSrs (type test-var))))
-    (is (= (triv/cps test-var) test-var))))
+    (is (extends? proto/PCpsTriv (type test-var)))
+    (is (not (extends? proto/PCpsSrs (type test-var))))
+    (is (= (proto/cps-triv test-var) test-var))))
 
 (let [test-fn-triv (Fn. [(Atomic. 'x)] (Atomic. 'x))]
   (deftest fn-triv
-    (is (extends? triv/PCpsTriv (type test-fn-triv)))
-    (is (not (extends? srs/PCpsSrs (type test-fn-triv))))
+    (is (extends? proto/PCpsTriv (type test-fn-triv)))
+    (is (not (extends? proto/PCpsSrs (type test-fn-triv))))
     (is (= (count (:fml* test-fn-triv)) 1))
-    (let [test-fn-cps (triv/cps test-fn-triv)]
+    (let [test-fn-cps (proto/cps-triv test-fn-triv)]
       (is (not (= test-fn-triv test-fn-cps)))
       (is (= (count (:fml* test-fn-cps)) 2))
       (is (instance? AppCont (:body test-fn-cps))))))
 
 (let [test-if-triv (IfTriv. (Atomic. 3) (Atomic. 4) (Atomic. 5))]
   (deftest if-triv
-    (is (extends? triv/PCpsTriv (type test-if-triv)))
-    (is (not (extends? srs/PCpsSrs (type test-if-triv))))
-    (let [test-if-cps (triv/cps test-if-triv)]
+    (is (extends? proto/PCpsTriv (type test-if-triv)))
+    (is (not (extends? proto/PCpsSrs (type test-if-triv))))
+    (let [test-if-cps (proto/cps-triv test-if-triv)]
       (is (= (:test test-if-triv) (:test test-if-cps)))
       (is (= (:conseq test-if-triv) (:conseq test-if-cps)))
       (is (= (:alt test-if-triv) (:alt test-if-cps)))
@@ -80,38 +77,35 @@
 
 (let [test-defn (Defn. 'id [(Fn. [(Atomic. 'x)] (Atomic. 'x))])]
   (deftest defn-triv
-    (is (extends? triv/PCpsTriv (type test-defn)))
-    (is (not (extends? srs/PCpsSrs (type test-defn))))
-    (let [test-defn-cps (triv/cps test-defn)]
+    (is (extends? proto/PCpsTriv (type test-defn)))
+    (is (not (extends? proto/PCpsSrs (type test-defn))))
+    (let [test-defn-cps (proto/cps-triv test-defn)]
       (is (instance? AppCont (:body (first (:func* test-defn-cps))))))))
 
 (let [test-op (SimpleOpTriv. '+ [(Atomic. 3) (Atomic. 4) (Atomic. 5)])]
   (deftest simple-op-triv
-    (is (extends? triv/PCpsTriv (type test-op)))
-    (is (not (extends? srs/PCpsSrs (type test-op))))
-    (let [test-op-cps (triv/cps test-op)]
+    (is (extends? proto/PCpsTriv (type test-op)))
+    (is (not (extends? proto/PCpsSrs (type test-op))))
+    (let [test-op-cps (proto/cps-triv test-op)]
       (for [opnd (:opnd* test-op)
             opnd-cps (:opnd* test-op-cps)]
         (is (= opnd opnd-cps))))))
 
 (let [test-app (App. (Fn. [(Atomic. 'x)] (Atomic. 'x)) [(Atomic. 5)])]
   (deftest app
-    (is (extends? srs/PCpsSrs (type test-app)))
-    (let [k (nv/new-var 'k)
-          app-cps (srs/cps test-app k)]
+    (is (extends? proto/PCpsSrs (type test-app)))
+    (let [k (util/new-var 'k)
+          app-cps (proto/cps-srs test-app k)]
       (is (instance? App app-cps)))))
 
 (let [test-if-srs (IfSrs. (SimpleOpTriv. 'zero? [(Atomic. 'x)])
                           (App. (Fn. [(Atomic. 'x)] (Atomic. 'x)) [(Atomic. 5)])
                           (Atomic. 12))]
   (deftest if-srs
-    (is (extends? srs/PCpsSrs (type test-if-srs)))
-    (is (extends? triv/PCpsTriv (type (:test test-if-srs))))
-    (is (extends? srs/PCpsSrs (type (:conseq test-if-srs))))
-    (is (extends? triv/PCpsTriv (type (:alt test-if-srs))))
-    (let [k (nv/new-var 'k)
-          test-if-cps (srs/cps test-if-srs k)]
-      #_(pprint test-if-cps))))
+    (is (extends? proto/PCpsSrs (type test-if-srs)))
+    (is (extends? proto/PCpsTriv (type (:test test-if-srs))))
+    (is (extends? proto/PCpsSrs (type (:conseq test-if-srs))))
+    (is (extends? proto/PCpsTriv (type (:alt test-if-srs))))))
 
 (let [test-if-srs (IfSrs. (App. (Fn. [(Atomic. 'x)]
                                      (SimpleOpTriv. 'zero? [(Atomic. 'x)]))
@@ -119,22 +113,16 @@
                           (App. (Fn. [(Atomic. 'x)] (Atomic. 'x)) [(Atomic. 5)])
                           (Atomic. 12))]
   (deftest if-srs2
-    (is (extends? srs/PCpsSrs (type test-if-srs)))
-    (is (extends? srs/PCpsSrs (type (:test test-if-srs))))
-    (is (extends? srs/PCpsSrs (type (:conseq test-if-srs))))
-    (is (extends? triv/PCpsTriv (type (:alt test-if-srs))))
-    (let [k (nv/new-var 'k)
-          test-if-cps (srs/cps test-if-srs k)]
-      #_(pprint test-if-cps))))
+    (is (extends? proto/PCpsSrs (type test-if-srs)))
+    (is (extends? proto/PCpsSrs (type (:test test-if-srs))))
+    (is (extends? proto/PCpsSrs (type (:conseq test-if-srs))))
+    (is (extends? proto/PCpsTriv (type (:alt test-if-srs))))))
 
 (let [test-op-srs (SimpleOpSrs. '+ [(Atomic. 3)
                                     (App. (Fn. [(Atomic. 'x)] (Atomic. 'x)) [(Atomic. 5)])
                                     (Atomic. 5)])]
   (deftest simple-op-srs
-    (is (extends? srs/PCpsSrs (type test-op-srs)))
-    (is (extends? triv/PCpsTriv (type (nth (:opnd* test-op-srs) 0))))
-    (is (extends? srs/PCpsSrs (type (nth (:opnd* test-op-srs) 1))))
-    (is (extends? triv/PCpsTriv (type (nth (:opnd* test-op-srs) 2))))
-    (let [k (nv/new-var 'k)
-          test-op-cps (srs/cps test-op-srs k)]
-      #_(pprint test-op-cps))))
+    (is (extends? proto/PCpsSrs (type test-op-srs)))
+    (is (extends? proto/PCpsTriv (type (nth (:opnd* test-op-srs) 0))))
+    (is (extends? proto/PCpsSrs (type (nth (:opnd* test-op-srs) 1))))
+    (is (extends? proto/PCpsTriv (type (nth (:opnd* test-op-srs) 2))))))
--- src/test/ctco/test/emit.clj
+++ src/test/ctco/test/emit.clj
@@ -1,54 +1,69 @@
-(ns bbc.test.emit
+;;----------------------------------------------------------------------
+;; File emit.clj
+;; Written by Chris Frisz
+;; 
+;; Created 15 Apr 2012
+;; Last modified 28 Apr 2012
+;; 
+;; Testing for the correctness of emit
+;;----------------------------------------------------------------------
+
+(ns ctco.test.emit
   (:use [clojure.test]
         [clojure.pprint]
-        [bbc.parse :only (parse)])
-  (:require [bbc.expr
+        [ctco.parse :only (parse)])
+  (:require [ctco.expr
              app atomic cont defn fn if simple-op]
-            [bbc.protocol
-             [pemit :as pemit]]
-            [bbc.util
-             [new-var :as nv]])
-  (:import [bbc.expr.app
+            [ctco.protocol :as proto])
+  (:import [ctco.expr.app
             App]
-           [bbc.expr.atomic
+           [ctco.expr.atomic
             Atomic]
-           [bbc.expr.cont
+           [ctco.expr.cont
             Cont AppCont AppContAbs]
-           [bbc.expr.defn
+           [ctco.expr.defn
             Defn]
-           [bbc.expr.fn
+           [ctco.expr.fn
             Fn]
-           [bbc.expr.if
+           [ctco.expr.if
             IfCps IfSrs IfTriv]
-           [bbc.expr.simple_op
+           [ctco.expr.simple_op
             SimpleOpCps SimpleOpSrs SimpleOpTriv]
-           [bbc.expr.thunk
+           [ctco.expr.thunk
             Thunk]))
 
 (deftest app-test
-  (is (= '(x y z) (pemit/emit (parse '(x y z)))))
-  (is (= '((clojure.core/fn [x] x) 5) (pemit/emit (parse '((fn [x] x) 5)))))
-  (is (= '((clojure.core/fn [x y z] (* x 3)) 7 3 12) (pemit/emit (parse '((fn [x y z] (* x 3)) 7 3 12))))))
+  (is (= '(x y z) (proto/emit (parse '(x y z)))))
+  (is (= '((clojure.core/fn [x] x) 5) (proto/emit (parse '((fn [x] x) 5)))))
+  (is (= '((clojure.core/fn [x y z] (* x 3)) 7 3 12)
+         (proto/emit (parse '((fn [x y z] (* x 3)) 7 3 12))))))
 
 (deftest atomic-test
-  (is (= 5 (pemit/emit (parse 5))))
-  (is (= 25883 (pemit/emit (parse 25883))))
-  (is (= 'stuff (pemit/emit (parse '(quote stuff))))))
+  (is (= 5 (proto/emit (parse 5))))
+  (is (= 25883 (proto/emit (parse 25883))))
+  (is (= '(quote stuff) (proto/emit (parse '(quote stuff))))))
  
 (deftest cont-test
-  (is (= '(clojure.core/fn [x] x) (pemit/emit (Cont. (Atomic. 'x) (Atomic. 'x)))))
-  (is (= '((clojure.core/fn [x] x) (quote thonk)) (pemit/emit (AppCont. (Cont. (Atomic. 'x) (Atomic. 'x)) (Atomic. (quote 'thonk)))))))
+  (is (= '(clojure.core/fn [x] x)
+         (proto/emit (Cont. (Atomic. 'x) (Atomic. 'x)))))
+  (is (= '((clojure.core/fn [x] x) (quote thonk))
+         (proto/emit (AppCont. (Cont. (Atomic. 'x) (Atomic. 'x))
+                               (Atomic. (quote 'thonk)))))))
 
 (deftest defn-test
-  (is (= '(clojure.core/defn id [x] x) (pemit/emit (parse '(defn id [x] x)))))
-  (is (= '(clojure.core/defn id ([x] x) ([x y] x)) (pemit/emit (parse '(defn id ([x] x) ([x y] x)))))))
+  (is (= '(clojure.core/defn id [x] x)
+         (proto/emit (parse '(defn id [x] x)))))
+  (is (= '(clojure.core/defn id ([x] x) ([x y] x))
+         (proto/emit (parse '(defn id ([x] x) ([x y] x)))))))
 
 
 (deftest if-test
-  (is (= '(if 3 4 5) (pemit/emit (parse '(if 3 4 5)))))
-  (is (= '(if ((clojure.core/fn [x] x) 3) 4 5) (pemit/emit (parse '(if ((fn [x] x) 3) 4 5))))))
+  (is (= '(if 3 4 5) (proto/emit (parse '(if 3 4 5)))))
+  (is (= '(if ((clojure.core/fn [x] x) 3) 4 5)
+         (proto/emit (parse '(if ((fn [x] x) 3) 4 5))))))
 
 ;; (deftest simple-op-test)
 
 (deftest thunk-test
-  (is (= '(clojure.core/fn [] (quote stuff)) (pemit/emit (Thunk. (Atomic. ''stuff))))))
+  (is (= '(clojure.core/fn [] (quote stuff))
+         (proto/emit (Thunk. (Atomic. ''stuff))))))

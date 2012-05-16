b6503005ab6f2548d321c99bd52708c713dc7b92
Chris Frisz <cjfrisz@indiana.edu>
2012-05-16 13:22:54 -0400

    Adding 'let' to the parser.
    With the LetCps, LetSrs, and LetTriv record types in the CTCO
    compiler, users can now include 'let' expressions in CTCO input. The
    parser has been updated to handle said input and generates appropriate
    record types based on the input.

--- src/main/ctco/parse.clj
+++ src/main/ctco/parse.clj
@@ -3,7 +3,7 @@
 ;; Written by Chris Frisz
 ;; 
 ;; Created 10 Apr 2012
-;; Last modified 28 Apr 2012
+;; Last modified 14 May 2012
 ;; 
 ;; Defines the parser for the Clojure TCO compiler.
 ;;----------------------------------------------------------------------
@@ -12,7 +12,7 @@
   (:use [clojure.core.match
          :only (match)])
   (:require [ctco.expr
-             app atomic defn fn if simple-op]
+             app atomic defn fn if let simple-op]
             [ctco.protocol :as proto]
             [ctco.util :as util])
   (:import [ctco.expr.app
@@ -25,10 +25,13 @@
             Fn]
            [ctco.expr.if
             IfCps IfSrs IfTriv]
+           [ctco.expr.let
+            LetCps LetSrs LetTriv]
            [ctco.expr.simple_op
             SimpleOpCps SimpleOpSrs SimpleOpTriv]))
 
-(declare parse parse-fn parse-defn parse-if parse-cond parse-op parse-app)
+(declare parse parse-fn parse-defn parse-if parse-cond parse-let parse-op
+         parse-app)
 
 (defn parse
   "Takes a sequence representing a Clojure expression (generally passed from a
@@ -48,6 +51,7 @@
     [(['defn name & func*] :seq)] (parse-defn name func*)
     [(['if test conseq alt] :seq)] (parse-if test conseq alt)
     [(['cond & clause*] :seq)] (parse-cond clause*)
+    [(['let bind* body] :seq)] (parse-let bind* body)
     [([(op :when util/simple-op?) & opnd*] :seq)] (parse-op op opnd*)
     [([rator & rand*] :seq)] (parse-app rator rand*)
     :else (throw (Exception. (str "Invalid expression in parse: " expr)))))
@@ -99,6 +103,16 @@
                       RCLAUSE* (nnext rclause*)]
                   (recur RCLAUSE* RST))))]
     (parse-rclause* (reverse clause*) (Atomic. nil))))
+
+(defn- parse-let
+  "Helper function for parse that handles 'let' expressions."
+  [bind* body]
+  (let [BIND* (vec (map parse bind*))
+        BODY (parse body)]
+    ;; Technically only need check inits, but no point in separating them
+    (if (or (some util/serious? BIND*) (util/serious? BODY))
+        (LetSrs. BIND* BODY)
+        (LetTriv. BIND* BODY))))
 
 (defn- parse-op
   "Helper function for parse that handles simple op expressions (e.g. +, -,

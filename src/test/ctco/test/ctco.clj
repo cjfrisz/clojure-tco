09f9f40df9824ccba688e063127b77326be69003
Chris Frisz <chris.frisz@gmail.com>
2012-06-09 21:24:02 -0400

    Adding new test harness for comparing normal and CTCO output.

--- src/test/ctco/test/ctco.clj
+++ src/test/ctco/test/ctco.clj
@@ -3,59 +3,46 @@
 ;; Written by Chris Frisz
 ;; 
 ;; Created 28 Apr 2012
-;; Last modified 28 Apr 2012
+;; Last modified 9 May 2012
 ;; 
 ;; Test programs for the full CTCO compiler.
 ;;----------------------------------------------------------------------
 
 (ns ctco.test.ctco
   (:use [clojure.test]
-        [ctco :only (ctco)]))
+        [clojure.core.match
+         :only (match)]
+        [clojure.walk
+         :only (prewalk-replace)]
+        [ctco
+         :only (ctco)]))
 
-(let [fact-seq '(defn fact
-                  [n]
-                  (if (zero? n)
-                      1
-                      (* n (fact (dec n)))))
-      inputs (range 10000)
-      fact (eval fact-seq)
-      fact-out (for [n inputs]
-                 (try
-                   (do
-                     (println (str "Running fact of " n))
-                     (fact (bigint n)))
-                   (catch Exception e
-                     (do
-                       (println "Stack overflow!")
-                       0))))
-      fact (eval `(ctco ~fact-seq))
-      fact-ctco-out (for [n inputs]
-                      (do
-                        (println (str "Running fact CTCO of " n))
-                        (fact (bigint n))))]
-  (deftest fact-test
-    (loop [fact-out fact-out
-           fact-ctco-out fact-ctco-out]
-      (when-not (and (nil? (seq fact-out)) (nil? (seq fact-ctco-out)))
-        (do
-          (is (or (= (first fact-out) (first fact-ctco-out))
-                  (zero? (first fact-ctco-out))))
-          (recur (next fact-out) (next fact-ctco-out)))))))
+(defmacro ctco-test
+  "Takes a valid CTCO expression and a sequence of inputs and evaluates the 
+  expression with each input in the sequence both with and without the CTCO
+  transformation. All StackOverflowErrors stemming from the unmodified code
+  are caught and reported. Exceptions from the CTCO modified code are uncaught
+  and assumed to be errors."
+  [expr & input*]
+  (let [ctco-expr (match [expr]
+                   [(['defn name fml* body] :seq)] 
+                    (prewalk-replace {name (gensym name)} expr)
+                   :else expr)]
+    (println input*)
+    `(let [old# ~expr
+           new# (ctco ~ctco-expr)]
+       (loop [input*# ~input*]
+         (when-not (nil? (seq input*#)) 
+           (let [fail# (gensym 'hukarz)
+                 input# (first input*#)
+                 old-eval# (try
+                             (apply old# input#)
+                             (catch StackOverflowError e#
+                               (println "Original overflowed on input" input#)
+                               fail#))
+                 new-eval# (apply new# input#)]
+             (if (not (= old-eval# fail#))
+               (is (= old-eval# new-eval#))
+               (println "CTCO completed and produced " new-eval#))
+             (recur (next input*#))))))))
 
-(let [ack-seq '(defn ack
-                 [m n]
-                 (cond
-                  (zero? m) (inc n)
-                  (and (> m 0) (zero? n)) (ack (dec m) 1)
-                  (and (> m 0) (> n 0) (ack (dec m) (ack m (dec n))))))])
-
-(let [myeven?-seq '(defn myeven?
-                           [n]
-                           (if (zero? n)
-                               true
-                               (myodd? (dec n))))
-      myodd?-seq '(defn myodd?
-                    [n]
-                    (if (zero? n)
-                        false
-                        (myeven? (dec n))))])

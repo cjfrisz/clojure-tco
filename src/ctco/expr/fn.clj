f20318eeaf537760bd3ab48842bfa8b4096f657f
Chris Frisz <chris.frisz@gmail.com>
2012-09-14 00:09:34 -0400

    Adding half-finished implementation for POverload to fn.

--- src/ctco/expr/fn.clj
+++ src/ctco/expr/fn.clj
@@ -3,7 +3,7 @@
 ;; Written by Chris Frisz
 ;; 
 ;; Created 30 Mar 2012
-;; Last modified 30 Aug 2012
+;; Last modified 14 Sep 2012
 ;; 
 ;; Defines the FnBody record type for representing 'fn' expressions in the
 ;; Clojure TCO compiler.
@@ -42,13 +42,6 @@
   proto/PAbstractK
     (abstract-k [this app-k]
       (proto/walk-expr this #(proto/abstract-k % app-k) nil))
-
-  proto/PUnparse
-    (unparse [this]
-      `(~(vec (map proto/unparse (:fml* this)))
-        ~@(let [cmap (:cmap this)]
-            (if cmap (list cmap) '()))
-        ~@(map proto/unparse (:bexpr* this))))
   
   proto/PCpsTriv
     (cps-triv [this]
@@ -64,6 +57,13 @@
     (thunkify [this]
       (proto/walk-expr this proto/thunkify nil))
 
+  proto/PUnparse
+    (unparse [this]
+      `(~(vec (map proto/unparse (:fml* this)))
+        ~@(let [cmap (:cmap this)]
+            (if cmap (list cmap) '()))
+        ~@(map proto/unparse (:bexpr* this))))
+
   proto/PWalkable
     (walk-expr [this f _]
       (FnBody. (:fml* this) (:cmap this) (vec (map f (:bexpr* this))))))
@@ -77,15 +77,34 @@
      (cps-triv [this]
        (proto/walk-expr this proto/cps-triv nil))
 
+   ;; NB: this assumes that the 'fn' has already undergone the CPS
+   ;; NB: transformation. this should probably be codified somehow
+   proto/POverload
+     (overload [this]
+       (letfn [(build-arity [orig* cps* n]
+                 (cond
+                  (nil? (seq orig*)) cps*
+                  (or (some #{n} orig*) (some #{n} cps*)) (recur orig-arity*
+                                                                 cps-arity*
+                                                                 (inc n))
+                  :else (recur (next orig*)
+                               (cons n cps*)
+                               (fnext orig-arity*))))]
+         (let [name (or (:name this) (util/new-var 'fn))
+               body* (:body* this)
+               orig-arity* (map (comp dec count :fml*) body*)
+               cps-arity* (build-arity orig-arity* '() (inc (first orig-arity*)))]
+           ---)))
+
+   proto/PThunkify
+     (thunkify [this]
+       (proto/walk-expr this proto/thunkify nil))
+
    proto/PUnparse
      (unparse [this]
        (let [name (:name this)]
          `(fn ~@(if name (list (proto/unparse name)) '())
             ~@(map proto/unparse (:body* this)))))
-
-   proto/PThunkify
-     (thunkify [this]
-       (proto/walk-expr this proto/thunkify nil))
 
    proto/PWalkable
      (walk-expr [this f _]

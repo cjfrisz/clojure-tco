d17bd7d7afb56fa783b6b2db03b84919d3538b44
Chris Frisz <cjfrisz@indiana.edu>
2012-10-14 10:54:05 -0400

    Adding protocol implementations to Recur.
    Adding implementations for PLoadTrampoline, PUnRecurify, and PWalkable
    to the Recur record type. This should allow for adding 'recur' as a
    source language expression.

--- src/ctco/expr/recur.clj
+++ src/ctco/expr/recur.clj
@@ -3,25 +3,47 @@
 ;; Written by Chris Frisz
 ;; 
 ;; Created 16 Apr 2012
-;; Last modified  6 Oct 2012
+;; Last modified 14 Oct 2012
 ;; 
 ;; Defines the Recur record type and operations for representing
 ;; 'recur' expressions in the Clojure TCO compiler.
 ;;
 ;; Recur implements the following protocols:
 ;;
+;;      PLoadTrampoline:
+;;              Maps load-tramp over the arguments to the 'recur' form.
+;;
 ;;      PUnparse:
 ;;              Unparses (recursively) the syntax for the expression as
 ;;              `(loop ~bind* ~body), where bind* is the vector of
 ;;              variables and bindings, and body is the body
 ;;              expression of the 'loop.'
+;;
+;;      PUnRecurify:
+;;              Replaces the 'recur' call with a function application.
+;;
+;;      PWalkable:
+;;              Maps a function over the arguments to the 'recur' form,
+;;              generating a new Recur record.
 ;;----------------------------------------------------------------------
 
 (ns ctco.expr.recur
   (:require [ctco.protocol :as proto]))
 
 (defrecord Recur [arg*]
+  proto/PLoadTrampoline
+  (load-tramp [this tramp]
+    (proto/walk-expr this f nil))
+
   proto/PUnparse
   (unparse [this]
     (let [arg* (map proto/unparse (:arg* this))]
-      `(recur ~@arg*))))
+      `(recur ~@arg*)))
+
+  proto/PUnRecurify
+  (unrecurify [this name]
+    (App. name (:arg* this)))
+
+  proto/PWalkable
+  (walk-expr [this f _]
+    (Recur. (mapv f (:arg* this)))))
